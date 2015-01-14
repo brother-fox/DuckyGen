59164880b2ce0d1df85a27e02e2874005d965b11
DragonHunter <dragoncodehunter@gmail.com>
2015-01-14 16:01:27 +0100

    Initial commit
    DuckyGen Initial Commit

---
+++ DuckyGen/Program.cs
@@ -0,0 +1,84 @@
+﻿using DuckyGenLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DuckyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DuckyGenerator gen = new DuckyGenerator();
+            gen.Delay = 750;
+            gen.AddDelay(1000); //execution delay
+
+            
+            //Your Script(s) Here
+            ChangeWallpaper("http://memekat.com/media/created/xr5kdq.jpg", gen);
+            HideDesktopIcons(gen);
+
+            
+            Console.WriteLine("SCRIPT: \r\n\r\n" + gen.GetScript());
+
+            //for me L:\ is directly writing to the DUCKY SdCard
+            Process.Start("L:\\");
+            Thread.Sleep(1000);
+            gen.Compile(@"C:\Users\Anguis\Desktop\DuckyEncoder\encoder.jar", @"L:\\inject.bin", "resources/us.properties");
+
+            Console.WriteLine("Inject.bin is compiled, have fun");
+            Console.ReadLine();
+        }
+
+        private static void DrawIlluminati(DuckyGenerator gen)
+        {
+            gen.PressWinKey('r', true);
+            gen.WriteString("notepad.exe");
+            gen.PressMenu(true);
+            gen.WriteStrings(File.ReadAllText(@"D:\Desktop\Files\illuminati.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None), true); //draw illuminati lol
+        }
+
+        private static void HideDesktopIcons(DuckyGenerator gen)
+        {
+            uint oldDelay = gen.Delay;
+            gen.Delay = 100; //fast execution
+            gen.PressWinKey('d', true);
+            gen.PressMenu(true);
+            gen.WriteCharacter('v');
+            gen.WriteCharacter('d');
+            gen.PressWinKey('d', true); //jump back to how it was, sneaky change yay
+            gen.Delay = oldDelay;
+        }
+
+        private static void ChangeWallpaper(string Path, DuckyGenerator gen)
+        {
+            //download image
+            gen.PressWinKey('r', true);
+            gen.WriteString(Path);
+            gen.PressEnter(true);
+            gen.PressCtrl("s");
+            gen.AddDelay(250);
+            gen.WriteString(@"%userprofile%\Pictures\uwotmate.jpg", true);
+            gen.PressEnter();
+            gen.AddDelay(250);
+
+            //if file exists, just overwrite
+            gen.PressLeftArrow();
+            gen.PressEnter();
+
+            //set wallpaper
+            gen.PressWinKey('r', true);
+            gen.WriteString(@"%userprofile%\Pictures\uwotmate.jpg", true);
+            gen.PressEnter(true);
+            gen.PressMenu();
+            gen.PressDownArrow();
+            gen.PressDownArrow();
+            gen.PressEnter();
+            gen.PressAlt("F4");
+            
+        }
+    }
+}
---
+++ DuckyGenLib/DuckyGenerator.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace DuckyGenLib
+{
+    public class DuckyGenerator
+    {
+        private StringBuilder SB;
+        public uint Delay { get; set; }
+
+        public DuckyGenerator()
+        {
+            SB = new StringBuilder();
+        }
+
+        public void WriteCharacter(char Char, bool AddDelay = false)
+        {
+            SB.AppendLine("STRING " + Char);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void WriteString(string Value, bool AddDelay = false)
+        {
+            if (String.IsNullOrEmpty(Value))
+                throw new ArgumentNullException("Value)");
+            SB.AppendLine("STRING " + Value);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        /// <summary>
+        /// Write multiple strings
+        /// </summary>
+        /// <param name="Values">The strings</param>
+        /// <param name="AddEnter">Add a enter after each string</param>
+        /// <param name="AddDelay">Add a delay after each string</param>
+        public void WriteStrings(string[] Values, bool AddEnter, bool AddDelay = false)
+        {
+            for (int i = 0; i < Values.Length; i++)
+            {
+                WriteString(Values[i], AddDelay);
+
+                if (AddEnter)
+                {
+                    PressEnter(false);
+                }
+            }
+        }
+
+        public void PressEnter(bool AddDelay = false)
+        {
+            SB.AppendLine("ENTER");
+
+            if (AddDelay)
+                this.AddDelay(this.Delay);
+        }
+
+        public void PressESC(bool AddDelay = false)
+        {
+            SB.AppendLine("ESC");
+
+            if (AddDelay)
+                this.AddDelay(this.Delay);
+        }
+
+        public void PressMenu(bool AddDelay = false)
+        {
+            SB.AppendLine("MENU");
+
+            if (AddDelay)
+                this.AddDelay(this.Delay);
+        }
+
+        /// <summary>
+        /// Press ALT + X at the same time for combined key presses
+        /// </summary>
+        /// <param name="Key"></param>
+        public void PressAlt(string Key, bool AddDelay = false)
+        {
+            SB.AppendLine("ALT " + Key);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        /// <summary>
+        /// Press CONTROL + X at the same time for combined key presses
+        /// </summary>
+        /// <param name="Key"></param>
+        public void PressCtrl(string Key, bool AddDelay = false)
+        {
+            SB.AppendLine("CONTROL " + Key);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        /// <summary>
+        /// Press SHIFT + X at the same time for combined key presses
+        /// </summary>
+        /// <param name="Key"></param>
+        public void PressShift(string Key, bool AddDelay = false)
+        {
+            SB.AppendLine("SHIFT " + Key);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        /// <summary>
+        /// Press WINDOWS_KEY + X at the same time for combined key presses, Also known as GUI
+        /// </summary>
+        /// <param name="Key"></param>
+        public void PressWinKey(char Key, bool AddDelay = false)
+        {
+            SB.AppendLine("GUI " + Key);
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void PressDownArrow(bool AddDelay = false)
+        {
+            SB.AppendLine("DOWNARROW");
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void PressUpArrow(bool AddDelay = false)
+        {
+            SB.AppendLine("UPARROW");
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void PressLeftArrow(bool AddDelay = false)
+        {
+            SB.AppendLine("LEFTARROW");
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void PressRightArrow(bool AddDelay = false)
+        {
+            SB.AppendLine("RIGHTARROW");
+
+            if (AddDelay)
+                this.AddDelay(Delay);
+        }
+
+        public void AddDelay(uint Delay)
+        {
+            SB.AppendLine("DELAY " + Delay);
+        }
+
+        /// <summary>
+        /// Compile the ducky script with the given Encoder
+        /// </summary>
+        /// <param name="DuckEncoderPath">The path to the encoder</param>
+        public void Compile(string DuckEncoderPath, string Output, string KeyboardLayout)
+        {
+            string Path = Environment.CurrentDirectory + "\\" + DateTime.Now.Ticks + ".txt";
+            File.WriteAllText(Path, GetScript());
+            FileInfo encoderFI = new FileInfo(DuckEncoderPath);
+
+            using (Process proc = new Process())
+            {
+
+                proc.StartInfo = new ProcessStartInfo("java", "-jar " + encoderFI.Name + " -i \"" + Path + "\" -o \"" + Output + "\" -l " + KeyboardLayout);
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.WorkingDirectory = encoderFI.DirectoryName;
+
+                if (!proc.Start())
+                {
+
+                }
+
+                proc.WaitForExit();
+            }
+        }
+
+        public string GetScript()
+        {
+            return SB.ToString();
+        }
+    }
+}
